51d0d37ffe3bda799197c724481f3a4fee121f68
Oleg Fedoseev <oleg.fedoseev@artezio.com>
2022-11-18 00:11:10 +0300

    Solved a problem "Longest Palindromic Substring"

---
+++ src/Algorithms/Medium/LongestPalindromicSubstring.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Medium
+{
+    public static class LongestPalindromicSubstring
+    {
+        public static string LongestPalindrome(string s)
+        {
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                var length = s.Length - i;
+
+                for (int j = 0; j <= i; j++)
+                {
+                    var isPalindrome = true;
+
+                    for (int k = 0; k < length / 2; k++)
+                    {
+                        if (s[j + k] != s[j + length - k - 1])
+                        {
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
+
+                    if (isPalindrome)
+                    {
+                        return s.Substring(j, length);
+                    }
+                }
+            }
+
+            return s[0].ToString();
+        }
+    }
+}
---
+++ tests/AlgorithmsTest/Medium/LongestPalindromicSubstring.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace AlgorithmsTest.Medium
+{
+    public class LongestPalindromicSubstring
+    {
+        [Theory]
+        [InlineData("babad", "bab")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("a", "a")]
+        [InlineData("aa", "aa")]
+        [InlineData("ab", "a")]
+        [InlineData("aaa", "aaa")]
+        [InlineData("asaa", "asa")]
+        public void Execute(string s, string expected)
+        {
+            var actual = Algorithms.Medium.LongestPalindromicSubstring.LongestPalindrome(s);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
